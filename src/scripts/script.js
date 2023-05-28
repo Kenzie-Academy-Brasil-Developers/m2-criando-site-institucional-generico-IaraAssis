a7319008d57163cb71d224c1f4cf91161717afb0
Iara Assis <iaraassisnega@gmail.com>
2023-05-27 22:26:16 -0300

    :tada: comit inicia

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,24 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function handModal(){
+
+    const button = document.querySelector('.header__button')
+    const modalController = document.querySelector('.modal__controller')
+
+    button.addEventListener('click', function() {
+        modalController.showModal()
+
+    closeModal()
+    })
+}
+
+function closeModal(){
+    
+    const button = document.querySelector('.modal__close')
+    const modalController = document.querySelector('.modal__controller')
+    
+    button.addEventListener('click', function(){
+        modalController.close()
+    })
+}
+
+handModal()
